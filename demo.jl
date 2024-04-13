8787e11dc9fd30d1ce8ecd64d985d30fcdeef774
Timothée Poisot <timothee.poisot@umontreal.ca>
2024-04-13 10:37:13 -0400

    df dimensions for ineff

--- demo.jl
+++ demo.jl
@@ -103,7 +103,7 @@ current_figure()
 save("02_conformal_prediction.png", current_figure())
 
 # Level at which the pixel is included in the range
-coverage_effect = DataFrame(α=Float64[], sure=Float64[], total=Float64[], coverage=Float64[], ssc=Float64[])
+coverage_effect = DataFrame(α=Float64[], sure=Float64[], total=Float64[], coverage=Float64[], ssc=Float64[], ineff=Float64[])
 surfacearea = cellsize(pred)
 for α in LinRange(0.0, 0.25, 8)
     partial_conf_model = conformal_model(tree; coverage=1 - α)
