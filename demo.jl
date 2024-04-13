983a56c0d8c609029f2145948f263ba2f7cec7f2
Timothée Poisot <timothee.poisot@umontreal.ca>
2024-04-12 20:15:58 -0400

    added the figure on pixel inclusion

--- demo.jl
+++ demo.jl
@@ -5,6 +5,8 @@ using ConformalPrediction
 using CairoMakie
 CairoMakie.activate!(; px_per_unit=2)
 
+bgc = colorant"#ecebe8ee"
+
 # Split the data to train a model
 schema(Xy)
 Xy.presence = coerce(Xy.presence, OrderedFactor)
@@ -52,12 +54,38 @@ evaluate(tree, X, y,
     verbosity=0
 )
 
+# Also fit a thresholded model
+point_tree = BinaryThresholdPredictor(tree, threshold=0.5)
+point_mach = machine(point_tree, X, y) |> fit!
+r = range(point_tree, :threshold, lower=0.1, upper=0.9)
+tuned_point_predictor = TunedModel(
+    point_tree,
+    tuning=RandomSearch(rng=12345),
+    resampling=CV(nfolds=6; rng=12345),
+    range = r,
+    measure=matthews_correlation,
+    n=30,
+)
+tuned_mach = machine(tuned_point_predictor, X, y) |> fit!
+threshold = report(tuned_mach).best_model.threshold
+
+
 # Get some prediction going
 pred = similar(temperature)
 pred.grid[findall(!isnothing, pred.grid)] .= pdf.(predict(mach, Xp), true)
-heatmap(pred, colormap=:navia)
+distrib = pred .>= threshold
 
-# Conformal prediction with changing coverage
+f = Figure(resolution=(1200, 500))
+ax = Axis(f[2, 1], aspect=DataAspect())
+heatmap!(ax, pred, colormap=[bgc, bgc])
+hm = heatmap!(ax, pred, colormap=:navia, colorrange=(0.0, 1.0))
+Colorbar(f[1, 1], hm; vertical=false, minorticksvisible=true, width=Relative(3.7/4))
+ax2 = Axis(f[2, 2], aspect=DataAspect())
+heatmap!(ax2, pred, colormap=[bgc, bgc])
+heatmap!(ax2, distrib, colormap=[:transparent, :black])
+current_figure()
+
+# Conformal prediction with a specific coverage rate
 α = 0.05
 conf_model = conformal_model(tree; coverage=1 - α)
 conf_mach = machine(conf_model, X, y)
@@ -65,9 +93,9 @@ fit!(conf_mach)
 
 evaluate!(
     conf_mach,
-    resampling=CV(nfolds=10, shuffle=true; rng=123),
+    resampling=CV(nfolds=10, shuffle=true; rng=12345),
     operation=predict,
-    measure=[emp_coverage, ssc]
+    measure=[emp_coverage, ineff, size_stratified_coverage]
 )
 
 # Make the uncertainty prediction
@@ -80,8 +108,6 @@ conf_false = [ismissing(p) ? nothing : (pdf(p, false) == 0 ? nothing : pdf(p, fa
 conformal.grid[findall(!isnothing, conformal.grid)] .= conf_val
 nopredict.grid[findall(!isnothing, nopredict.grid)] .= conf_false
 
-bgc = colorant"#ecebe8ee"
-
 f = Figure(resolution=(1200, 500))
 ax = Axis(f[2, 1], aspect=DataAspect())
 heatmap!(ax, pred, colormap=[bgc, bgc])
@@ -92,6 +118,47 @@ heatmap!(ax2, pred, colormap=[bgc, bgc])
 heatmap!(ax2, conformal, colormap=[:black, :black])
 heatmap!(ax2, mask((conformal .> 0), (nopredict .> 0)), colormap=[:grey, :grey])
 current_figure()
+
+# Level at which the pixel is included in the range
+included_at = similar(pred)
+sure_at = similar(pred)
+replace!(sure_at, 0.0 => Inf)
+replace!(included_at, 0.0 => -Inf)
+valid_positions = findall(!isnothing, pred.grid)
+
+for α in LinRange(0.0, 0.12, 30)
+    partial_conf_model = conformal_model(tree; coverage=1 - α)
+    partial_conf_mach = machine(partial_conf_model, X, y)
+    fit!(partial_conf_mach)
+    partial_conf_pred = predict(partial_conf_mach, Xp)
+    partial_conf_val = [ismissing(p) ? nothing : (pdf(p, true) == 0 ? nothing : pdf(p, true)) for p in partial_conf_pred]
+    partial_conf_false = [ismissing(p) ? nothing : (pdf(p, false) == 0 ? nothing : pdf(p, false)) for p in partial_conf_pred]
+    # Pixels that are true only
+    sure_pixels = setdiff(findall(!isnothing, partial_conf_val), findall(!isnothing, partial_conf_false))
+    unsure_pixels = findall(!isnothing, partial_conf_val)
+    # Sure/unsure pixels update
+    sure_at.grid[valid_positions[filter(x -> sure_at.grid[valid_positions[x]] > α, sure_pixels)]] .= α
+    included_at.grid[valid_positions[filter(x -> included_at.grid[valid_positions[x]] < α, unsure_pixels)]] .= α
+end
+
+replace!(sure_at, Inf => nothing)
+replace!(included_at, -Inf => nothing)
+
+f = Figure(resolution=(1200, 500))
+ax2 = Axis(f[2, 1], aspect=DataAspect())
+ax = Axis(f[2, 2], aspect=DataAspect())
+heatmap!(ax, pred, colormap=[bgc, bgc])
+hm = heatmap!(ax, sure_at, colormap=:glasgow, colorrange=(0.0, 0.12))
+Colorbar(f[1, 1], hm; vertical=false, minorticksvisible=true, width=Relative(3.7/4))
+heatmap!(ax2, pred, colormap=[bgc, bgc])
+heatmap!(ax2, included_at, colormap=:glasgow, colorrange=(0.0, 0.12))
+current_figure()
+
+# Masks for the Shapley values
+unsure_mask = mask((conformal .> 0), (nopredict .> 0))
+sure_mask = conformal .> 0
+sure_mask.grid[findall(!isnothing, nopredict.grid)] .= nothing
+
 
 #pres = findall(isequal(true), Xy.presence)
 #scatter!(Xy.longitude[pres], Xy.latitude[pres], markersize=2, color=:teal)
@@ -119,11 +186,6 @@ expvar.grid[findall(!isnothing, expvar.grid)] .= vx
 
 expl = similar(conformal)
 expl.grid[findall(!isnothing, expl.grid)] .= vy
-
-# Masks for the Shapley values
-unsure_mask = mask((conformal .> 0), (nopredict .> 0))
-sure_mask = conformal .> 0
-sure_mask.grid[findall(!isnothing, nopredict.grid)] .= nothing
 
 frange = maximum(abs.(extrema(vy))) .* (-1, 1)
 f = Figure()
