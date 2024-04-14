5c8e6a84f6e221ad4b4c3113dded55209a069ec0
Timothée Poisot <timothee.poisot@umontreal.ca>
2024-04-14 15:22:21 -0400

    update figures up to (not incl.) shapley

--- demo.jl
+++ demo.jl
@@ -2,11 +2,15 @@ include("_getdata.jl")
 
 using MLJ
 using ConformalPrediction
+using ColorSchemes
 using CairoMakie
 CairoMakie.activate!(; px_per_unit=2)
 
-# Background color for the maps - this is the color for NODATA
-bgc = colorant"#ecebe8ee"
+# Some info on color for the entire figures
+mapbg = colorant"#ecebe8ee"
+probacolor = ColorSchemes.navia
+rangecolor = ColorSchemes.ColorScheme(reverse(ColorSchemes.bukavu.colors))
+effectcolor = ColorSchemes.managua
 
 # Split the data to train a model
 #schema(Xy) # Check the type of the different inputs
@@ -68,14 +72,18 @@ pred = similar(temperature)
 pred.grid[findall(!isnothing, pred.grid)] .= pdf.(predict(mach, Xp), true)
 distrib = pred .>= threshold
 
-f = Figure(resolution=(1200, 500))
-ax = Axis(f[2, 1], aspect=DataAspect())
-heatmap!(ax, pred, colormap=[bgc, bgc])
-hm = heatmap!(ax, pred, colormap=:navia, colorrange=(0.0, 1.0))
-Colorbar(f[1, 1], hm; vertical=false, minorticksvisible=true, width=Relative(3.7 / 4))
-ax2 = Axis(f[2, 2], aspect=DataAspect())
-heatmap!(ax2, pred, colormap=[bgc, bgc])
-heatmap!(ax2, distrib, colormap=[:transparent, :black])
+fig_brt = Figure(resolution=(1200, 500))
+ax_pred = Axis(fig_brt[2, 1], aspect=DataAspect())
+ax_range = Axis(fig_brt[2, 2], aspect=DataAspect())
+heatmap!(ax_pred, pred, colormap=[mapbg, mapbg])
+hm = heatmap!(ax_pred, pred, colormap=probacolor, colorrange=(0.0, 1.0))
+Colorbar(fig_brt[1, 1], hm; vertical=false, minorticksvisible=true, width=Relative(3.7 / 4))
+heatmap!(ax_range, pred, colormap=[mapbg, mapbg])
+heatmap!(ax_range, distrib, colormap=rangecolor)
+legcol = [ColorSchemes.get(rangecolor, x, extrema(distrib)) for x in sort(unique(values(distrib)))]
+leglab = ["absence", "presence"]
+legbox = [PolyElement(color = c, strokecolor = :black, strokewidth=1) for c in legcol]
+Legend(fig_brt[1,2], legbox, leglab; orientation = :horizontal, tellheight=false, tellwidth=false, halign=:center, valign=:center, nbanks=1, framevisible=false)
 current_figure()
 save("01_brt_prediction.png", current_figure())
 
@@ -94,23 +102,36 @@ evaluate!(
 
 # Make the uncertainty prediction
 conformal = similar(temperature)
-nopredict = similar(temperature)
+confindex = findall(!isnothing, conformal.grid)
+conforange = similar(temperature)
 conf_pred = predict(conf_mach, Xp)
-conf_val = [ismissing(p) ? nothing : (pdf(p, true) == 0 ? nothing : pdf(p, true)) for p in conf_pred]
+conf_true = [ismissing(p) ? nothing : (pdf(p, true) == 0 ? nothing : pdf(p, true)) for p in conf_pred]
 conf_false = [ismissing(p) ? nothing : (pdf(p, false) == 0 ? nothing : pdf(p, false)) for p in conf_pred]
+conf_either = [any(isnothing.([conf_true[i], conf_false[i]])) ? nothing : true for i in eachindex(conf_true)]
 
-conformal.grid[findall(!isnothing, conformal.grid)] .= conf_val
-nopredict.grid[findall(!isnothing, nopredict.grid)] .= conf_false
+conformal.grid[findall(!isnothing, conformal.grid)] .= conf_true
 
-f = Figure(resolution=(1200, 500))
-ax = Axis(f[2, 1], aspect=DataAspect())
-heatmap!(ax, pred, colormap=[bgc, bgc])
-hm = heatmap!(ax, conformal, colormap=:navia, colorrange=(0.0, 1.0))
-Colorbar(f[1, 1], hm; vertical=false, minorticksvisible=true, width=Relative(3.7 / 4))
-ax2 = Axis(f[2, 2], aspect=DataAspect())
-heatmap!(ax2, pred, colormap=[bgc, bgc])
-heatmap!(ax2, conformal, colormap=[:black, :black])
-heatmap!(ax2, mask((conformal .> 0), (nopredict .> 0)), colormap=[:grey, :grey])
+conforange.grid[confindex[findall(!isnothing, conf_true)]] .= 3
+conforange.grid[confindex[findall(!isnothing, conf_false)]] .= 0
+# Split between things in/out of BRT range
+conforange.grid[confindex[findall(!isnothing, conf_either)]] .= 1
+inrange = keys(mask(distrib, mask(conforange .== 1, conforange)))
+for ir in inrange
+    conforange[ir] = 2
+end
+
+
+fig_conformal = Figure(resolution=(1200, 500))
+ax_confpred = Axis(fig_conformal[2, 1], aspect=DataAspect())
+ax_confrange = Axis(fig_conformal[2, 2], aspect=DataAspect())
+heatmap!(ax_confpred, pred, colormap=[mapbg, mapbg])
+hm = heatmap!(ax_confpred, conformal, colormap=probacolor, colorrange=(0.0, 1.0))
+Colorbar(fig_conformal[1, 1], hm; vertical=false, minorticksvisible=true, width=Relative(3.7 / 4))
+heatmap!(ax_confrange, conforange, colormap=rangecolor)
+legcol = [ColorSchemes.get(rangecolor, x, extrema(conforange)) for x in sort(unique(values(conforange)))]
+leglab = ["absence", "uncertain (out)", "uncertain (in)", "presence"]
+legbox = [PolyElement(color = c, strokecolor = :black, strokewidth=1) for c in legcol]
+Legend(fig_conformal[1,2], legbox, leglab; orientation = :horizontal, tellheight=false, tellwidth=false, halign=:center, valign=:center, nbanks=1, framevisible=false)
 current_figure()
 save("02_conformal_prediction.png", current_figure())
 
@@ -139,21 +160,21 @@ for α in LinRange(0.0, 0.25, 20)
     push!(coverage_effect, (α, s, u, evl.measurement[1], evl.measurement[2], evl.measurement[3]))
 end
 
-f = Figure(resolution=(1200, 500))
-ax = Axis(f[1:2, 1], yscale=sqrt, ylabel="Area (km²)", xlabel="Risk level (α)")
-ax2 = Axis(f[2, 2], ylabel="Coverage", xlabel="Risk level (α)",yaxisposition=:right)
-ax3 = Axis(f[1, 2], ylabel="Inefficiency", xlabel=" ",yaxisposition=:right, xaxisposition=:top)
-scatterlines!(ax, coverage_effect.α, 1e-3 .* coverage_effect.total, label="Total range", color=:grey, linestyle=:dash)
-scatterlines!(ax, coverage_effect.α, 1e-3 .* coverage_effect.sure, label="Certain range", color=:black)
-scatterlines!(ax2, coverage_effect.α, coverage_effect.coverage, label="Empirical", color=:black)
-scatterlines!(ax2, coverage_effect.α, coverage_effect.ssc, label="Size stratified", color=:black, marker=:utriangle)
-scatterlines!(ax3, coverage_effect.α, coverage_effect.ineff, color=:black)
-axislegend(ax)
-axislegend(ax2)
-linkxaxes!(ax, ax2)
-linkxaxes!(ax, ax3)
-tightlimits!(ax2)
-tightlimits!(ax3)
+fig_risk = Figure(resolution=(1200, 500))
+ax_area = Axis(fig_risk[1:2, 1], yscale=sqrt, ylabel="Area (km²)", xlabel="Risk level (α)")
+ax_cov = Axis(fig_risk[2, 2], ylabel="Coverage", xlabel="Risk level (α)",yaxisposition=:right)
+ax_ineff = Axis(fig_risk[1, 2], ylabel="Inefficiency", xlabel=" ",yaxisposition=:right, xaxisposition=:top)
+scatterlines!(ax_area, coverage_effect.α, 1e-3 .* coverage_effect.total, label="Total range", color=:grey, linestyle=:dash)
+scatterlines!(ax_area, coverage_effect.α, 1e-3 .* coverage_effect.sure, label="Certain range", color=:black)
+scatterlines!(ax_cov, coverage_effect.α, coverage_effect.coverage, label="Empirical", color=:black)
+scatterlines!(ax_cov, coverage_effect.α, coverage_effect.ssc, label="Size stratified", color=:black, marker=:utriangle)
+scatterlines!(ax_ineff, coverage_effect.α, coverage_effect.ineff, color=:black)
+axislegend(ax_area)
+axislegend(ax_cov)
+linkxaxes!(ax_area, ax_cov)
+linkxaxes!(ax_area, ax_ineff)
+tightlimits!(ax_cov)
+tightlimits!(ax_ineff)
 current_figure()
 save("03_coverage_effect.png", current_figure())
 
